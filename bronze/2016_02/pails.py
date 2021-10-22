d603b44f611d839a749e09ce193dc75e5bb3911b
Dazhi Jiao <9097689+jiaola@users.noreply.github.com>
2021-10-21 21:42:33 -0400

    pails

--- bronze/2016_02/pails.py
+++ bronze/2016_02/pails.py
@@ -3,13 +3,13 @@ import sys
 sys.stdin = open("pails.in", "r")
 sys.stdout = open("pails.out", "w")
 
-a, b, c = map(int, input().split())
+x, y, m = map(int, input().split())
 mx = 0
-for i in range(0, c // a + 1):
-    for j in range(0, c // b + 1):
-        m = i * a + j * b
-        if m <= c:
-            mx = max(mx, m)
+for i in range(0, m // x + 1):
+    for j in range(0, m // y + 1):
+        z = i * x + j * y
+        if z <= m:
+            mx = max(mx, z)
         else:
             break
 print(mx)
